08f8f4ce9f82f39bfa5fcb4555294fd643b49f28
Girish Jammu <girishraina.jammu@gmail.com>
2021-09-20 16:45:50 -0400

    posts

---
+++ src/pages/posts.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const PostsPage = () => (
+  <Layout>
+    List of all posts
+  </Layout>
+)
+
+export default PostsPage
