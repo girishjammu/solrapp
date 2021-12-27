154a49b38fdc34d096f46cb462cab4a721216061
Girish <girishraina.jammu@gmail.com>
2021-12-27 16:01:15 -0500

    solr-app

---
+++ api/src/main/java/com/girishjamu/solrapp/SolrappApplication.java
@@ -0,0 +1,13 @@
+package com.girishjamu.solrapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolrappApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/girishjamu/solrapp/SolrappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.girishjamu.solrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolrappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
